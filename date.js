40bc68bc9e1ddf68ed400c88f1ed9ff600f84e03
lizbirnel <lizbirnel@byui.edu>
2021-01-10 00:20:53 -0700

    last modified again

--- date.js
+++ date.js
@@ -1,3 +1,3 @@
 let d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
-document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("lastModified").innerHTML = Date.parse(document.lastModified);

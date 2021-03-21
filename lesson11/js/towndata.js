157539bd38341cf130bf8cdc97fc7b547f60f0fe
lizbirnel <lizbirnel@byui.edu>
2021-03-21 00:47:40 -0600

    console.log

--- lesson11/js/towndata.js
+++ lesson11/js/towndata.js
@@ -5,7 +5,7 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject); //temporary checking for valid response and data parsing
+        //console.table(jsonObject); //temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
         //towns.shift();
         //towns.splice(1,3);
